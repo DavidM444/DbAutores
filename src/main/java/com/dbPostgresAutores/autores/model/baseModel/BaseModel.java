f429cea6b86831bedc83199511dc9d2c48cb8927
DavidM444 <david541gamer@gmail.com>
2024-03-25 18:19:48 -0500

    getters and setters were added to store Actor entities

--- src/main/java/com/dbPostgresAutores/autores/model/baseModel/BaseModel.java
+++ src/main/java/com/dbPostgresAutores/autores/model/baseModel/BaseModel.java
@@ -2,6 +2,7 @@ package com.dbPostgresAutores.autores.model.baseModel;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 
 import java.io.Serializable;
 
@@ -11,4 +12,28 @@ public class BaseModel extends BaseUpdate{
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseModel{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
