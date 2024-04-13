90bd2da69180ff0e7041826c99e00ec3420e1a61
DavidM444 <david541gamer@gmail.com>
2024-04-13 16:06:06 -0500

    language, category and actor entity test creation.

---
+++ src/test/java/com/dbPostgresAutores/autores/testControllers/LanguageCategoryActorTest.java
@@ -0,0 +1,70 @@
+package com.dbPostgresAutores.autores.testControllers;
+
+import com.dbPostgresAutores.autores.model.Category;
+import com.dbPostgresAutores.autores.model.Language;
+import com.dbPostgresAutores.autores.model.actorEnt.Actor;
+import com.dbPostgresAutores.autores.services.repository.ActorRepository;
+import com.dbPostgresAutores.autores.services.repository.CategoryRepository;
+import com.dbPostgresAutores.autores.services.repository.LanguageRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.checkerframework.checker.units.qual.K;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import java.time.LocalDate;
+import java.util.function.Supplier;
+
+@SpringBootTest
+public class LanguageCategoryActorTest {
+    private final LanguageRepository languageRepository;
+    private final CategoryRepository categoryRepository;
+    private final ActorRepository actorRepository;
+
+    @Autowired
+    public LanguageCategoryActorTest(LanguageRepository languageRepository, CategoryRepository categoryRepository, ActorRepository actorRepository) {
+        this.languageRepository = languageRepository;
+        this.categoryRepository = categoryRepository;
+        this.actorRepository = actorRepository;
+    }
+
+
+
+    @Test
+    void saveLanguageEntity(){
+        Language language = new Language();
+        language.setName("Spanish");
+
+        Language languageResponse = languageRepository.save(language);
+        Assertions.assertNotNull(languageResponse);
+        Assertions.assertEquals(languageResponse.getId(),1);
+        Assertions.assertEquals(language.getName(),languageResponse.getName());
+    }
+
+    @Test
+    void saveCategoryEntity(){
+        Category category = new Category();
+        category.setName("Action");
+        Category categoryResponse = categoryRepository.save(category);
+
+        Assertions.assertEquals(category.getId(),categoryResponse.getId());
+        Assertions.assertNotNull(categoryResponse);
+    }
+
+    @Test
+    void saveActorEntity(){
+        Actor actor = new Actor();
+        actor.setFirstName("Penelope");
+        actor.setLastName("Guiness");
+
+        Actor actorResponse = actorRepository.save(actor);
+        Assertions.assertNotNull(actorResponse);
+
+        Supplier<String> name = ()-> "Penelope";
+        Assertions.assertEquals(actorResponse.getFirstName(), name.get());
+    }
+
+}
