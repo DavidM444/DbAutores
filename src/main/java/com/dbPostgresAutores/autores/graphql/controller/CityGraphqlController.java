cc0ba7e2511ef77a142204478fa5fd797351b4a3
DavidM444 <david541gamer@gmail.com>
2024-04-26 19:37:58 -0500

    subscription graphql.

--- src/main/java/com/dbPostgresAutores/autores/graphql/controller/CityGraphqlController.java
+++ src/main/java/com/dbPostgresAutores/autores/graphql/controller/CityGraphqlController.java
@@ -1,19 +1,20 @@
-package com.dbPostgresAutores.autores.controller.graphqlController;
+package com.dbPostgresAutores.autores.graphql.controller;
 
-import com.dbPostgresAutores.autores.model.place.City;
 import com.dbPostgresAutores.autores.model.place.Country;
 import com.dbPostgresAutores.autores.services.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.*;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Controller
 public class CityGraphqlController {
@@ -22,26 +23,27 @@ public class CityGraphqlController {
     private CountryRepository countryRepository;
 
     @QueryMapping
-    public List<Country> listCountries(){
+    public List<Country> listCountries() {
         return countryRepository.findAll();
     }
 
     //@SchemaMapping(typeName = "Query", field = "hello") == @QueryMapping
     @QueryMapping
-    private Country findByCountry(@Argument Integer id){
+    private Country findByCountry(@Argument Integer id) {
         Optional<Country> country = countryRepository.findById(id);
         return country.orElse(null);
 
     }
 
-
     @QueryMapping
-    Flux<Country> fluxCountry(){return Flux.fromIterable(countryRepository.findAll());}
+    Flux<Country> fluxCountry() {
+        return Flux.fromIterable(countryRepository.findAll());
+    }
 
     //using batch
     @BatchMapping
-    Map<Country, Account> account(List<Country> countries){
-        System.out.println("llamando cuenta para "+countries.size()+ " countries");
+    Map<Country, Account> account(List<Country> countries) {
+        System.out.println("llamando cuenta para " + countries.size() + " countries");
         return countries.stream()
                 .collect(Collectors.toMap(country -> country,
                         country -> new Account(country.getId())));
@@ -54,20 +56,28 @@ public class CityGraphqlController {
         return Mono.just(new Account(country.getId()));
     }*/
 
-    @SchemaMapping(typeName = "Query", field = "hello")
-    private String hello(){
-        return "Hello";
+    //subscription
+    @SubscriptionMapping
+    Flux<Account> greetings() {
+        return Flux.fromStream(Stream.generate(
+                () -> new Account(new Random().nextInt(8))
+        )).delayElements(Duration.ofSeconds(1)).take(6);
     }
 
+    @SchemaMapping(typeName = "Query", field = "hello")
+    private String hello() {
+        return "Hello";
+    }
 
     //take de value
     private final AtomicInteger atomicInteger = new AtomicInteger();
 
-    @MutationMapping //== to @SchemaMapping(typeName = "Mutation",  field = "insertCountry")
-    Country insertCountry(@Argument String name){
+    @MutationMapping
+        //== to @SchemaMapping(typeName = "Mutation",  field = "insertCountry")
+    Country insertCountry(@Argument String name) {
         var id = this.atomicInteger.incrementAndGet(); //take value from last id in db and add 1
-        System.out.println("id atomic "+id);
-        System.out.println("id atomic 2 "+atomicInteger.getPlain());
+        System.out.println("id atomic " + id);
+        System.out.println("id atomic 2 " + atomicInteger.getPlain());
         return countryRepository.save(new Country(name));
     }
 
@@ -77,6 +87,6 @@ public class CityGraphqlController {
     }*/
 }
 
-record Account(Integer id){
+record Account(Integer id) {
 }
 
